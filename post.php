90d305bac8fb9de270d337e0697fcbaca878ce1d
naofujita <116623701+naofujita@users.noreply.github.com>
2022-11-01 12:30:24 +0900

    最新

---
+++ post.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>sample</title>
+</head>
+
+<body>
+
+    <h2>問合せ内容</h2>
+
+<?php
+//お問い合わせ内容
+$other = $_POST['other'];
+
+//お名前
+$name = $_POST['name'];
+
+//性別
+$name = $_POST['gender'];
+
+
+
+//電話番号
+$name = $_POST['tel'];
+
+//メールアドレス
+$name = $_POST['email']; ?>
+
+<input type="submit" value="送信" />
+
+</body>
+</html>
