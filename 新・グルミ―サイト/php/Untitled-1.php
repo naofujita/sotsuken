ab9bae9b7544cb3998e7275b17c6ea0cbe20f3b3
naofujita <116623701+naofujita@users.noreply.github.com>
2022-11-09 15:11:32 +0900

    Add files via upload
    11/09 最新
    接続の仕方（藤田）→index2.php

--- 新・グルミ―サイト/php/Untitled-1.php
+++ 新・グルミ―サイト/php/Untitled-1.php
@@ -2,14 +2,13 @@
 
 // セッションの開始
 session_start();
-   
+
 $contact = htmlspecialchars($_POST['contact'], ENT_QUOTES, 'UTF-8');
 $comment = htmlspecialchars($_POST['comment'], ENT_QUOTES, 'UTF-8');
 $name = htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8');
 $gender = htmlspecialchars($_POST['gender'], ENT_QUOTES, 'UTF-8');
 $tel = htmlspecialchars($_POST['tel'],ENT_QUOTES, 'UTF-8');
 $email = htmlspecialchars($_POST['email'],ENT_QUOTES, 'UTF-8');
-
 
 // 入力値をセッション変数に格納
 $_SESSION['contact'] = $contact;
@@ -61,7 +60,7 @@ $_SESSION['email'] = $email;
     <tr><th>お問い合わせの種類</th><td><?php echo $contact; ?></td></tr>
     <tr><th>お問い合わせ内容</th><td><?php echo $comment; ?></td></tr>
     <tr><th>お名前</th><td><?php echo $name; ?></td></tr>
-    <tr><th>性別</th><td><?php echo $gender; ?></td></tr>
+    <tr><th>性別</th><td><?php echo $ruby; ?></td></tr>
     <tr><th>電話番号（半角）</th><td><?php echo $email; ?></td></tr>
     <tr><th>メールアドレス</th><td><?php echo $tel; ?></td></tr>
     
--- 新・グルミ―サイト/php/index2.php
+++ 新・グルミ―サイト/php/index2.php
@@ -1,54 +1,42 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>sample</title>
-</head>
-
-<body>
-
-    <h2>お問合せ内容</h2>
-
-    <form action="mailto.php" method="post">
-
-        <table border="1">
-            <tr>
-                <td>お問い合わせ内容</td>
-                <td><?php echo $_POST['other']; ?></td>
-            </tr>
-            <tr>
-                <td>名前</td>
-                <td><?php echo $_POST['name']; ?></td>
-            </tr>
-            <tr>
-                <td>性別</td>
-                <td><?php echo $_POST['gender']; ?></td>
-            </tr>
-            <tr>
-                <td>電話番号</td>
-                <td><?php echo $_POST['tel']; ?></td>
-            </tr>
-            <tr>
-                <td>メールアドレス</td>
-                <td><?php echo $_POST['email']; ?></td>
-            </tr>
-            
-        </table>
-
-        <input type="submit" value="送信" />
-    </form>
-
-</body>
-
-</html>
-
-
-
-$name = $_POST['gender'];
-if ($gender == "man"){
-  $gender = "男性";
-}
-else if ($gender == "woman"){
-  $gender = "女性";
-}
+<?php
+ //データベースへ接続する
+ //dbnameでデータベースの名前(gourmmy),hostは基本localhostでOK
+ $dsn = 'mysql:dbname=gourmmy;host=localhost';
+ $user = 'root';//user名
+ $password = '';//パスワード(rootはデフォルトで何も設定されていない)
+ //tryでエラーが出ても安全に終了できるようにする
+ try{
+ //データベースへ接続する
+ $dbh = new PDO($dsn, $user, $password);
+ //テーブルenquiryから全てのデータを取り出すSQL
+ $sql = 'select * from enquiry';
+?>
+ <table>
+ <tr><th>id</th><th>contact</th><th>comment</th>
+ <th>name</th><th>gender</th><th>name</th>
+ <th>email</th></tr><br>
+<?php
+ //全てのデータを取り出すおまじない
+ //phpMyAdminで全て確認できるので覚える必要はほぼない
+ foreach ($dbh->query($sql) as $row) {
+ print("<tr>");
+ print("<td>" . $row['id'] . "</td>");
+ print("<td>" . $row['contact'] . "</td>");
+ print("<td>" . $row['comment'] . "</td>");
+ print("<td>" . $row['name'] . "</td>");
+ print("<td>" . $row['gender'] . "</td>");
+ print("<td>" . $row['tel'] . "</td>");
+ print("<td>" . $row['email'] . "</td>");
+ print('</tr><br>');
+ }
+?>
+ </table>
+<?php
+ //エラーが出た時に⾏われる処理
+ }catch (PDOException $e){
+ print('Error:'.$e->getMessage());
+ die();
+ }
+ //接続を解除する
+ $dbh = null;
+?>
