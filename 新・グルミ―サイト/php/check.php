c3fcfa6595f1dd916fb38ed2152e4534eb001477
kubo0629 <116697610+kubo0629@users.noreply.github.com>
2022-11-15 13:35:00 +0900

    Add files via upload

--- 新・グルミ―サイト/php/check.php
+++ 新・グルミ―サイト/php/check.php
@@ -1,10 +1,10 @@
 <?php
-require("./dbconnect.php");
+require("./connect.php");
 session_start();
  
 /* 会員登録の手続き以外のアクセスを飛ばす */
 if (!isset($_SESSION['join'])) {
-    header('Location: entry.php');
+    header('Location: aka.php');
     exit();
 }
  
@@ -13,16 +13,17 @@ if (!empty($_POST['check'])) {
     $hash = password_hash($_SESSION['join']['password'], PASSWORD_BCRYPT);
  
     // 入力情報をデータベースに登録
-    $statement = $db->prepare("INSERT INTO members SET name=?, mail=?, password=?");
+    $statement = $db->prepare("INSERT INTO touroku SET name=?, name_furi=?, mail=?, pass=?, repass=?, yuubin=?, address=?, phone=?, birth=?");
     $statement->execute(array(
-        $_SESSION['join']['name1'],
-        $_SESSION['join']['name2'],
+        $_SESSION['join']['name'],
+        $_SESSION['join']['name_furi'],
         $_SESSION['join']['mail'],
-        $_SESSION['join']['password'],
-        $_SESSION['join']['post code'],
-        $_SESSION['join']['adress'],
-        $_SESSION['join']['tel'],
-        $_SESSION['join']['birth date'],
+        $_SESSION['join']['pass'],
+        $_SESSION['join']['repass'],
+        $_SESSION['join']['yuubin'],
+        $_SESSION['join']['address'],
+        $_SESSION['join']['phone'],
+        $_SESSION['join']['birth'],
         $hash
     ));
  
@@ -55,12 +56,12 @@ if (!empty($_POST['check'])) {
  
             <div class="control">
                 <p>名前</p>
-                <p><span class="fas fa-angle-double-right"></span> <span class="check-info"><?php echo htmlspecialchars($_SESSION['join']['name1'], ENT_QUOTES); ?></span></p>
+                <p><span class="fas fa-angle-double-right"></span> <span class="check-info"><?php echo htmlspecialchars($_SESSION['join']['name'], ENT_QUOTES); ?></span></p>
             </div>
  
             <div class="control">
                 <p>名前(ふりがな)</p>
-                <p><span class="fas fa-angle-double-right"></span> <span class="check-info"><?php echo htmlspecialchars($_SESSION['join']['name2'], ENT_QUOTES); ?></span></p>
+                <p><span class="fas fa-angle-double-right"></span> <span class="check-info"><?php echo htmlspecialchars($_SESSION['join']['name_furi'], ENT_QUOTES); ?></span></p>
             </div>
  
             <div class="control">
@@ -70,31 +71,36 @@ if (!empty($_POST['check'])) {
 
             <div class="control">
                 <p>パスワード</p>
-                <p><span class="fas fa-angle-double-right"></span> <span class="check-info"><?php echo htmlspecialchars($_SESSION['join']['password'], ENT_QUOTES); ?></span></p>
+                <p><span class="fas fa-angle-double-right"></span> <span class="check-info"><?php echo htmlspecialchars($_SESSION['join']['pass'], ENT_QUOTES); ?></span></p>
             </div>
  
             <div class="control">
+                <p>パスワード</p>
+                <p><span class="fas fa-angle-double-right"></span> <span class="check-info"><?php echo htmlspecialchars($_SESSION['join']['repass'], ENT_QUOTES); ?></span></p>
+            </div>
+
+            <div class="control">
                 <p>郵便番号</p>
-                <p><span class="fas fa-angle-double-right"></span> <span class="check-info"><?php echo htmlspecialchars($_SESSION['join']['post code'], ENT_QUOTES); ?></span></p>
+                <p><span class="fas fa-angle-double-right"></span> <span class="check-info"><?php echo htmlspecialchars($_SESSION['join']['yuubin'], ENT_QUOTES); ?></span></p>
             </div>
  
             <div class="control">
                 <p>住所</p>
-                <p><span class="fas fa-angle-double-right"></span> <span class="check-info"><?php echo htmlspecialchars($_SESSION['join']['adress'], ENT_QUOTES); ?></span></p>
+                <p><span class="fas fa-angle-double-right"></span> <span class="check-info"><?php echo htmlspecialchars($_SESSION['join']['address'], ENT_QUOTES); ?></span></p>
             </div>
  
             <div class="control">
                 <p>電話番号</p>
-                <p><span class="fas fa-angle-double-right"></span> <span class="check-info"><?php echo htmlspecialchars($_SESSION['join']['tel'], ENT_QUOTES); ?></span></p>
+                <p><span class="fas fa-angle-double-right"></span> <span class="check-info"><?php echo htmlspecialchars($_SESSION['join']['phone'], ENT_QUOTES); ?></span></p>
             </div>
  
             <div class="control">
                 <p>生年月日</p>
-                <p><span class="fas fa-angle-double-right"></span> <span class="check-info"><?php echo htmlspecialchars($_SESSION['join']['birth date'], ENT_QUOTES); ?></span></p>
+                <p><span class="fas fa-angle-double-right"></span> <span class="check-info"><?php echo htmlspecialchars($_SESSION['join']['birth'], ENT_QUOTES); ?></span></p>
             </div>
  
             <br>
-            <a href="entry.php" class="back-btn">変更する</a>
+            <a href="aka.php" class="back-btn">変更する</a>
             <button type="submit" class="btn next-btn">登録する</button>
             <div class="clear"></div>
         </form>
