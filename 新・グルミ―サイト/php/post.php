50247ce88c3d4bdce0dd96af8c6fe82dd9d7d0db
naofujita <116623701+naofujita@users.noreply.github.com>
2022-11-17 01:34:36 +0900

    Add files via upload

--- 新・グルミ―サイト/php/post.php
+++ 新・グルミ―サイト/php/post.php
@@ -1,31 +1,22 @@
 
 
 <?php
- //データ定義
- $enquiryTypeList = ["注⽂について","配達について","決済について","その他"];
- $genderList = ["男","⼥"];
- //データを取得
- $enquiry = $_POST["enquiry"];
- $comment = $_POST["comment"];
- $name = $_POST["name"];
- $gender = $_POST["gender"];
- $tel = $_POST["tel"];
- $email = $_POST["email"];
+
  //正しく取得できているかの確認
  //print($name . ":" . $genderList[$gender] . ":" . 
  //$phone . ":" . $mail . ":" . $enquiry . ":" 
  //. $enquiryTypeList[$enquiry_type]);
-
  $dsn = 'mysql:dbname=gourmmy;host=localhost';
  $user = 'root';
  $password = '';
  try{
  $dbh = new PDO($dsn, $user, $password);
- $sql = "INSERT INTO enquiry(id, enquiry, comment, name, gender, tel, email, ) VALUES(?,?,?,?,?,?,?)";
+ $sql = "INSERT INTO enquery(id,enquiry_type,enquiry,name, gender, tel, email, ) 
+ VALUES(?,?,?,?,?,?,?)";
  //SQLインジェクション対策
  $stmt = $dbh->prepare($sql);
- $flag = $stmt->execute(array(NULL,$name,$genderList[$gender],$tel,
- $email,$enquiry,$enquiryTypeList[$enquiry]));
+ $flag = $stmt->execute(array(NULL,$enquiryTypeList[$enquiry_type],$enquiry,$name,
+ $genderList[$gender],$tel,$email));
  //正しくSQLが処理されたかの確認
  if($flag){
  print("完了");
