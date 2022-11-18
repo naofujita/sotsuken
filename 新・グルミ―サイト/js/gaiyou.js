65e75e718680055e3bc448ec861c94638565cb34
MAO <116697582+huzimoto5@users.noreply.github.com>
2022-11-18 12:36:19 +0900

    Add files via upload

---
+++ 新・グルミ―サイト/js/gaiyou.js
@@ -0,0 +1,10 @@
+bubbly({
+    colorStart: '#ffffff',
+    colorStop: '#ffffff',
+    blur: 1,
+    compose: 'source-over',
+    bubbleFunc:() => `hsla(${Math.random() * 50}, 100%, 50%, .3)`
+  });
+
+
+  
--- 新・グルミ―サイト/js/index.js
+++ 新・グルミ―サイト/js/index.js
@@ -5,3 +5,18 @@ bubbly({
     compose: 'source-over',
     bubbleFunc:() => `hsla(${Math.random() * 50}, 100%, 50%, .3)`
   });
+
+
+  $(function () {
+    $(window).scroll(function () {
+      const windowHeight = $(window).height();
+      const scroll = $(window).scrollTop();
+  
+      $('.element').each(function () {
+        const targetPosition = $(this).offset().top;
+        if (scroll > targetPosition - windowHeight + 100) {
+          $(this).addClass("is-fadein");
+        }
+      });
+    });
+  });
