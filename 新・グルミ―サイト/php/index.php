dbfa48b12e75c5d795f2fdda8a4105f9ae7fbe41
naofujita <116623701+naofujita@users.noreply.github.com>
2022-11-22 09:23:16 +0900

    Add files via upload

--- 新・グルミ―サイト/php/index.php
+++ 新・グルミ―サイト/php/index.php
@@ -3,65 +3,6 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>内容確認</title>
-<link href="css/toiawase.css" rel="stylesheet" type="text/css">
-</head>
-<body>
-    
-    <header id="header" class="wrapper">
-        <h3 class="gourmmy">Gourmmy</h3>
-          <nav>
-            <ul>
-                <li><a href="index.html">HOME</a></li>
-				<li><a href="gaiyou.html">Gourmmyについて</a></li>
-                <li><a href="toiawase2.html">問い合わせ</a></li>
-				<li><a href="login.html">サインイン</a></li>
-           </ul>
-         </nav>
-        </header>
-        
-
-<br>
-<br>
-<br>
-
-<h1 class="midasi">内容確認</h1>
-<p style="text-align : center ; font-size: 2rem; margin-top: 30px; ;">＜お問い合わせ内容をご確認の上、「送信」ボタンをクリックしてください。＞</p>
-<br>
-<br>
-
-<?php
-
-// セッションの開始
-session_start();
-$genderList = ["男","女"];
-$contactList = ["注文について","配達について","決済について","その他"];
-$contact = htmlspecialchars($_POST['enquiry_type'], ENT_QUOTES, 'UTF-8');
-$comment = htmlspecialchars($_POST['enquiry'], ENT_QUOTES, 'UTF-8');
-$name = htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8');
-$gender = htmlspecialchars($_POST['gender'], ENT_QUOTES, 'UTF-8');
-$tel = htmlspecialchars($_POST['tel'],ENT_QUOTES, 'UTF-8');
-$email = htmlspecialchars($_POST['email'],ENT_QUOTES, 'UTF-8');
-
-
-// 入力値をセッション変数に格納
-// データベースの設定ができるようになったらいらないよ
-// 確認だけならセッションで管理するとデータを盗まれる可能性あり！！
-/* $_SESSION['contact'] = $contact;
-$_SESSION['comment'] = $comment;
-$_SESSION['name'] = $name;
-$_SESSION['gender'] = $gender;
-$_SESSION['tel'] = $tel;
-$_SESSION['email'] = $email;
- */
-
-?>
-
-
-<!DOCTYPE HTML>
-<html lang="ja">
-<head>
-<meta charset="utf-8">
 <title>問い合わせ確認画面 | Gourmmy</title>
 <style>
 
@@ -85,22 +26,93 @@ $_SESSION['email'] = $email;
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script type="text/javascript" src="../css/animation1.js"></script>
 </head>
-
 <body>
+    
+    <header id="header" class="wrapper">
+        <h3 class="gourmmy">Gourmmy</h3>
+          <nav>
+            <ul>
+                <li><a href="../index.html">HOME</a></li>
+				<li><a href="../gaiyou.html">Gourmmyについて</a></li>
+                <li><a href="../toiawase.html">問い合わせ</a></li>
+				<li><a href="../login.html">サインイン</a></li>
+           </ul>
+         </nav>
+        </header>
+        
+
+<br>
+<br>
+<br>
+
+<h1 class="midasi">内容確認</h1>
+<p style="text-align : center ; font-size: 2rem; margin-top: 30px; ;">＜お問い合わせ内容をご確認の上、「送信」ボタンをクリックしてください。＞</p>
+<br>
+<br>
+
+<?php
+
+// セッションの開始
+session_start();
+$contactList = ["注文について","配達について","決済について","その他"];
+$contact = htmlspecialchars($_POST['enquiry_type'], ENT_QUOTES, 'UTF-8');
+$enquiry = htmlspecialchars($_POST['enquiry'], ENT_QUOTES, 'UTF-8');
+$name = htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8');
+$gender = htmlspecialchars($_POST['gender'], ENT_QUOTES, 'UTF-8');
+$tel = htmlspecialchars($_POST['tel'],ENT_QUOTES, 'UTF-8');
+$email = htmlspecialchars($_POST['email'],ENT_QUOTES, 'UTF-8');
+
+
+// 入力値をセッション変数に格納
+// データベースの設定ができるようになったらいらないよ
+// 確認だけならセッションで管理するとデータを盗まれる可能性あり！！
+/* $_SESSION['contact'] = $contact;
+$_SESSION['comment'] = $comment;
+$_SESSION['name'] = $name;
+$_SESSION['gender'] = $gender;
+$_SESSION['tel'] = $tel;
+$_SESSION['email'] = $email;
+ */
+
+?>
     <div class="box_con02">
     <form action="post.php" method="post">
-    <table class="formTable">
-    
-    <tr><th>お問い合わせの種類</th><td><?php echo $enquiry_typeList[$enquiry_type]; ?></td></tr>
-    <tr><th>お問い合わせ内容</th><td><?php echo $enquiry; ?></td></tr>
-    <tr><th>お名前</th><td><?php echo $name; ?></td></tr>
-    <tr><th>性別</th><td><?php echo $gender; ?></td></tr>
-    <tr><th>電話番号（半角）</th><td><?php echo $tel; ?></td></tr>
-    <tr><th>メールアドレス</th><td><?php echo $email; ?></td></tr>
-    
-    <!--echoで変数に格納された値を表示-->
-    
-    </table>
+        <table class="formTable">       
+            <tr>
+                <th>お問い合わせの種類</th>
+                <!-- 配列の名前が違ってたよ -->
+                <td><?php echo $contactList[$contact]; ?></td>
+                <input type="hidden" name="enquiry_type" value="<?= $contactList[$contact]; ?>">
+            </tr>                         
+            <tr>
+                <th>お問い合わせ内容</th>
+                <!-- $_POSTで受け取った変数名が違うよ -->
+                <td><?php echo $enquiry; ?></td>
+                <input type="hidden" name="enquiry" value="<?= $enquiry; ?>">
+            </tr>
+            <tr>
+                <th>お名前</th>
+                <td><?php echo $name; ?></td>
+                <input type="hidden" name="name" value="<?= $name; ?>">
+            </tr>
+            <tr>
+                <th>性別</th>
+                <td><?php echo $gender; ?></td>
+                <input type="hidden" name="gender" value="<?= $gender; ?>">
+            </tr>
+            <tr>
+                <th>電話番号（半角）</th>
+                <td><?php echo $tel; ?></td>
+                <input type="hidden" name="tel" value="<?= $tel; ?>">
+            </tr>
+            <tr>
+                <th>メールアドレス</th>
+                <td><?php echo $email; ?></td>
+                <input type="hidden" name="email" value="<?= $email; ?>">
+            </tr>
+        
+        <!--echoで変数に格納された値を表示-->
+        </table>
     <br><br>
         
         <div class="btn" style="margin-bottom: 20%;    font-size: 2rem;   transform: scale(1.5)">
--- 新・グルミ―サイト/php/post.php
+++ 新・グルミ―サイト/php/post.php
@@ -1,90 +1,8 @@
-
-<!doctype html>
-<html lang="ja">
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>内容確認</title>
-<link href="css/toiawase.css" rel="stylesheet" type="text/css">
-</head>
-<body>
-    
-    <header id="header" class="wrapper">
-        <h3 class="gourmmy">Gourmmy</h3>
-          <nav>
-            <ul>
-                <li><a href="index.html">HOME</a></li>
-				<li><a href="gaiyou.html">Gourmmyについて</a></li>
-                <li><a href="toiawase2.html">問い合わせ</a></li>
-				<li><a href="login.html">サインイン</a></li>
-           </ul>
-         </nav>
-        </header>
-        
-
-<br>
-<br>
-<br>
-
-<h1 class="midasi">お問い合わせ完了</h1>
-<p style="text-align : center ; font-size: 2rem; margin-top: 30px;">＜内容が送信されました!内容を確認後、ご入力いただいたメールアドレスへ返信いたします。
-<br>なお、返信には数日かかる場合がございます。＞</p>
-<br>
-<br>
-
-
-
-<?php
-//エラーの解決11/21 "60error"
-if(isset($_POST['enquiry'])){
-    $genderList = ["男","女"];
-    $enquiryTypeList = ["注文について","配達について","決済について","その他"];
-   
-    //データを取得
-    $name = $_POST["name"];
-    $gender = $_POST["gender"];
-    $tel = $_POST["tel"];
-    $email = $_POST["email"];
-    $enquiry = $_POST["enquiry"];
-    $enquiry_type = $_POST["enquiry_type"];
-    
-     //正しく取得できているかの確認
-     //print($name . ":" . $genderList[$gender] . ":" . 
-     //$phone . ":" . $mail . ":" . $enquiry . ":" 
-     //. $enquiryTypeList[$enquiry_type]);
-     $dsn = 'mysql:dbname=gourmmy;host=localhost';
-     $user = 'root';
-     $password = '';
-     try{
-     $dbh = new PDO($dsn, $user, $password);
-     $sql = "INSERT INTO enquery(id,enquiry_type,enquiry, name, gender, tel, email) 
-     VALUES(?,?,?,?,?,?,?)";
-     
-    
-     }
-     //SQLインジェクション対策
-     $stmt = $dbh->prepare($sql);
-     $flag = $stmt->execute(array(NULL,$enquiryTypeList[$enquiry_type],$enquiry,$name,$genderList[$gender],$tel,$email));
-     //正しくSQLが処理されたかの確認
-     if($flag){
-     print("完了");
-     }else{
-     print("エラー");
-     }
-    }catch (PDOException $e){
-     print('Error:'.$e->getMessage());
-     die();
-     }
-     $dbh = null;
-    }
-    ?>
-        
-
-
 <!DOCTYPE HTML>
 <html lang="ja">
 <head>
 <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>問い合わせ完了画面 | Gourmmy</title>
 <style>
 
@@ -108,13 +26,78 @@ if(isset($_POST['enquiry'])){
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script type="text/javascript" src="../css/animation1.js"></script>
 </head>
-
 <body>
+    
+    <header id="header" class="wrapper">
+        <h3 class="gourmmy">Gourmmy</h3>
+        <nav>
+            <ul>
+                <li><a href="../index.html">HOME</a></li>
+				<li><a href="../gaiyou.html">Gourmmyについて</a></li>
+                <li><a href="../toiawase.html">問い合わせ</a></li>
+				<li><a href="../login.html">サインイン</a></li>
+            </ul>
+        </nav>
+    </header> 
+<br>
+<br>
+<br>
+
+<h1 class="midasi">お問い合わせ完了</h1>
+<p style="text-align : center ; font-size: 2rem; margin-top: 30px;">
+＜内容が送信されました!内容を確認後、ご入力いただいたメールアドレスへ返信いたします。
+<br>なお、返信には数日かかる場合がございます。＞
+</p>
+<br>
+<br>
+
+<?php
+    //エラーの解決11/21 "60error"
+    //エラーの原因変なところに}が入っていたり数があっていなかった(よくあるミス)
+    //これは発見難しいからわしもよくミスる
+    if(isset($_POST['enquery'])){
+    
+        //データを取得
+        $name = $_POST["name"];
+        $gender = $_POST["gender"];
+        $tel = $_POST["tel"];
+        $email = $_POST["email"];
+        $enquiry = $_POST["enquiry"];
+        $enquiry_type = $_POST["enquiry_type"];
+        
+        //正しく取得できているかの確認
+        print($name . ":" . $gender . ":" . 
+        $tel . ":" . $email . ":" . $enquiry . ":" 
+        . $enquiry_type);
+        $dsn = 'mysql:dbname=gourmmy;host=localhost';
+        $user = 'root';
+        $password = '';
+        try{
+            $dbh = new PDO($dsn, $user, $password);
+            //enquiryがenqueryになってたよ...
+            $sql = "INSERT INTO enquery(id,enquiry_type,enquiry, name, gender, tel, email) 
+            VALUES(?,?,?,?,?,?,?)";
+            //SQLインジェクション対策
+            $stmt = $dbh->prepare($sql);
+            $flag = $stmt->execute(array(NULL,$enquiry_type,$enquiry,$name,$gender,$tel,$email));
+            //正しくSQLが処理されたかの確認
+            if(!$flag){
+                print("エラー");
+            }
+        }catch(PDOException $e){
+            print('Error:'.$e->getMessage());
+            die();
+        }
+        $dbh = null;
+    }else{
+        print("送信されてません。<br>お手数お掛けしますが、公式の電話番号までご連絡ください。");
+    }
+    ?>
+        
     <h1>
-    <div class="btn" style="margin-bottom: 20%;    font-size: 2rem;   transform: scale(1.5)">
-    <span><a href="index.html"></a><input type="submit" value="HOMEへ戻る" disabled id="subm" /></span>
+    <div class="btn" style="margin-top:20px;  margin-bottom: 20%;    font-size: 2rem;   transform: scale(1.5)">
+    <span><a href="../index.html"><input type="submit" value="HOMEへ戻る" id="subm" /></a></span>
     </div>
-</form>
 </div>
 </body>
 </html>
