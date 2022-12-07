bfdc28b8f5909ac70d8ba40b10228113ab029dd5
MAO <116697582+huzimoto5@users.noreply.github.com>
2022-12-07 12:30:16 +0900

    Add files via upload

--- 新・グルミ―サイト/js/index2.js
+++ 新・グルミ―サイト/js/index2.js
@@ -26,3 +26,31 @@ $(function () {
     });
   });
 });
+
+
+$(function () {
+  const fade_bottom = 50; // 画面下からどの位置でフェードさせるか(px)
+  const fade_move = 100; // どのぐらい要素を動かすか(px)
+  const fade_time = 800; // フェードの時間(ms)
+  // フェード前のcssを定義
+  $(".scroll-fade-side").css({
+    opacity: 0,
+    transform: "translateX(" + fade_move + "px)",
+    transition: fade_time + "ms",
+  });
+  // スクロールまたはロードするたびに実行
+  $(window).on("scroll load", function () {
+    const scroll_top = $(this).scrollTop();
+    const scroll_bottom = scroll_top + $(this).height();
+    const fade_position = scroll_bottom - fade_bottom;
+    $(".scroll-fade-side").each(function () {
+      const this_position = $(this).offset().top;
+      if (fade_position > this_position) {
+        $(this).css({
+          opacity: 1,
+          transform: "translateX(0)",
+        });
+      }
+    });
+  });
+});
