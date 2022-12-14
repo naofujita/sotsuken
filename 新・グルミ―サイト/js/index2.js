8f8c563d2dd32b972f46517f9efff68ae3114b42
MAO <116697582+huzimoto5@users.noreply.github.com>
2022-12-14 09:41:33 +0900

    Add files via upload

--- 新・グルミ―サイト/js/index2.js
+++ 新・グルミ―サイト/js/index2.js
@@ -54,3 +54,46 @@ $(function () {
     });
   });
 });
+
+
+$(function () {
+  const fade_bottom = 50; // 画面下からどの位置でフェードさせるか(px)
+  const fade_move = 100; // どのぐらい要素を動かすか(px)
+  const fade_time = 1500; // フェードの時間(ms)
+  // フェード前のcssを定義
+  $(".scroll-fade-up").css({
+    opacity: 0,
+    transform: "translateY(" + fade_move + "px)",
+    transition: fade_time + "ms",
+  });
+  // スクロールまたはロードするたびに実行
+  $(window).on("scroll load", function () {
+    const scroll_top = $(this).scrollTop();
+    const scroll_bottom = scroll_top + $(this).height();
+    const fade_position = scroll_bottom - fade_bottom;
+    $(".scroll-fade-up").each(function () {
+      const this_position = $(this).offset().top;
+      if (fade_position > this_position) {
+        $(this).css({
+          opacity: 1,
+          transform: "translateY(0)",
+        });
+      }
+    });
+  });
+});
+
+$(function(){
+  //読み込みが完了したら実行する
+  $(window).on('load',function(){
+    //ローディングアニメーションをフェードアウト
+    $('.loader').delay(600).fadeOut(600);
+    //背景色をフェードアウト
+    $('.loader-bg').delay(900).fadeOut(800);
+});
+
+  //ページの読み込みが完了してなくても5秒後にアニメーションを非表示にする
+  setTimeout(function(){
+    $('.loader-bg').fadeOut(600);
+  },5000);
+});
