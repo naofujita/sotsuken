bdcce8ba811e23287f577054e4e7abbaaf0ec411
MAO <116697582+huzimoto5@users.noreply.github.com>
2023-02-10 12:38:54 +0900

    Add files via upload

--- 新・グルミ―サイト/js/Mewdo curry.js
+++ 新・グルミ―サイト/js/Mewdo curry.js
@@ -1,11 +1,18 @@
-bubbly({
-    colorStart: '#ffffff',
-    colorStop: '#ffffff',
-    blur: 1,
-    compose: 'source-over',
-    bubbleFunc:() => `hsla(${Math.random() * 50}, 100%, 50%, .3)`
+$(function(){
+    //読み込みが完了したら実行する
+    $(window).on('load',function(){
+      //ローディングアニメーションをフェードアウト
+      $('.loader').delay(600).fadeOut(600);
+      //背景色をフェードアウト
+      $('.loader-bg').delay(900).fadeOut(800);
   });
-
+  
+    //ページの読み込みが完了してなくても5秒後にアニメーションを非表示にする
+    setTimeout(function(){
+      $('.loader-bg').fadeOut(600);
+    },5000);
+  });
+  
 
 
   <script>
